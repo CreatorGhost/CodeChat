45b9a92248f26be702efc7fa2846b4582f6d37b1
Aditya Pratap Singh <cybercreed010@gmail.com>
2023-10-05 12:51:51 +0530

    Update streamlit_app.py
    Made it to use gpt-4 and a python developer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,18 @@
-# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
-
 import openai
 import streamlit as st
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+    openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+    if openai.api_key:
         if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [{"role": "system", "content": "You are a helpful assistant that's a Python developer with experience in DevOps.You provide the approch by which we can solve the problem along with the code.Along with the time and space complexity "}]
+
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -30,7 +26,7 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder = st.empty()
         full_response = ""
         for response in openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
             messages=[{"role": m["role"], "content": m["content"]}
                       for m in st.session_state.messages], stream=True):
             full_response += response.choices[0].delta.get("content", "")
